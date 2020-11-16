782e0b212c266b0a0eb8d26682deb1dcfd342831
N e o <paul.gardien@epitech.eu>
2020-11-16 10:51:53 +0100

    Initial Commit

---
+++ Misty/Misty/Misty.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Misty
+{
+    partial class Misty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Misty));
+            this.launch = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.reduce = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.directory = new System.Windows.Forms.FolderBrowserDialog();
+            this.uncompress = new System.Windows.Forms.Button();
+            this.file_mist = new System.Windows.Forms.OpenFileDialog();
+            this.bar = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // launch
+            // 
+            this.launch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.launch.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.launch.ForeColor = System.Drawing.Color.DarkViolet;
+            this.launch.Location = new System.Drawing.Point(99, 91);
+            this.launch.Name = "launch";
+            this.launch.Size = new System.Drawing.Size(233, 28);
+            this.launch.TabIndex = 10;
+            this.launch.Text = "Compress";
+            this.launch.UseVisualStyleBackColor = true;
+            this.launch.Click += new System.EventHandler(this.launch_Click);
+            // 
+            // status
+            // 
+            this.status.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.ForeColor = System.Drawing.Color.DarkViolet;
+            this.status.Location = new System.Drawing.Point(12, 178);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(400, 23);
+            this.status.TabIndex = 13;
+            this.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // reduce
+            // 
+            this.reduce.BackgroundImage = global::Misty.Properties.Resources.minimize;
+            this.reduce.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.reduce.FlatAppearance.BorderSize = 0;
+            this.reduce.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reduce.Location = new System.Drawing.Point(376, 13);
+            this.reduce.Name = "reduce";
+            this.reduce.Size = new System.Drawing.Size(15, 15);
+            this.reduce.TabIndex = 12;
+            this.reduce.UseVisualStyleBackColor = true;
+            this.reduce.Click += new System.EventHandler(this.reduce_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackgroundImage = global::Misty.Properties.Resources.exit;
+            this.exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.Location = new System.Drawing.Point(397, 12);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(15, 15);
+            this.exit.TabIndex = 9;
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Released by Neo";
+            // 
+            // uncompress
+            // 
+            this.uncompress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.uncompress.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uncompress.ForeColor = System.Drawing.Color.DarkViolet;
+            this.uncompress.Location = new System.Drawing.Point(99, 136);
+            this.uncompress.Name = "uncompress";
+            this.uncompress.Size = new System.Drawing.Size(233, 28);
+            this.uncompress.TabIndex = 15;
+            this.uncompress.Text = "Uncompress";
+            this.uncompress.UseVisualStyleBackColor = true;
+            this.uncompress.Click += new System.EventHandler(this.uncompress_Click);
+            // 
+            // file_mist
+            // 
+            this.file_mist.Filter = ".mis|";
+            // 
+            // bar
+            // 
+            this.bar.BackColor = System.Drawing.Color.DarkViolet;
+            this.bar.Location = new System.Drawing.Point(0, 215);
+            this.bar.Name = "bar";
+            this.bar.Size = new System.Drawing.Size(0, 10);
+            this.bar.TabIndex = 16;
+            // 
+            // Misty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(24)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(424, 225);
+            this.ControlBox = false;
+            this.Controls.Add(this.bar);
+            this.Controls.Add(this.uncompress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.reduce);
+            this.Controls.Add(this.launch);
+            this.Controls.Add(this.exit);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(24)))), ((int)(((byte)(37)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Misty";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button launch;
+        private System.Windows.Forms.Button reduce;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog directory;
+        private System.Windows.Forms.Button uncompress;
+        private System.Windows.Forms.OpenFileDialog file_mist;
+        private System.Windows.Forms.Panel bar;
+    }
+}
+
---
+++ Misty/Misty/Misty.cs
@@ -0,0 +1,250 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Misty
+{
+    public partial class Misty : Form
+    {
+        public Misty()
+        {
+            InitializeComponent();
+        }
+
+        private Task UILogs(string new_string)
+        {
+            status.Text = new_string;
+            status.Refresh();
+
+            return (Task.CompletedTask);
+        }
+
+        private Task boxer(string message, string title, MessageBoxIcon type)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            MessageBox.Show(message, title, buttons, type);
+            return (Task.CompletedTask);
+        }
+
+        private int count_char(string str, char c)
+        {
+            int total = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                    total++;
+            }
+            return (total);
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private string crop(string message)
+        {
+            if (count_char(message, '\\') >= 3)
+            {
+                return ($"{message.Split('\\')[0]}\\{message.Split('\\')[1]}\\...\\{message.Split('\\')[count_char(message, '\\')]}");
+            }
+
+            return (message);
+        }
+
+        private int get_folder()
+        {
+            directory.ShowDialog();
+            if (directory.SelectedPath.Length == 0)
+                return (-1);
+            else
+                UILogs(crop(directory.SelectedPath)).Wait();
+
+            return (0);
+        }
+
+        private string get_full_path(string path, int minimum)
+        {
+            string full = "";
+            int slash = 0;
+
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == '\\')
+                    slash++;
+                if (slash >= minimum)
+                    full += path[i];
+            }
+            return (full);
+        }
+
+        private Task progress_bar(int percent)
+        {
+            int max = 424;
+            int progress = (percent * max) / 100;
+
+            bar.Size = new Size(progress, 10);
+            bar.Refresh();
+
+            return (Task.CompletedTask);
+        }
+
+        private Task compress(string output)
+        {
+            string[] files = Directory.GetFiles(directory.SelectedPath, "*.*", SearchOption.AllDirectories);
+            Console.WriteLine(files.Length);
+            string name = null;
+            int progress = 0;
+            int total = count_char(directory.SelectedPath, '\\');
+            Dictionary<string, byte[]> content = new Dictionary<string, byte[]>();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                progress = (i * 100) / files.Length;
+                name = get_full_path(files[i], total);
+                UILogs($"{name}").Wait();
+                progress_bar(progress).Wait();
+                content.Add($"{name}", File.ReadAllBytes(files[i]));
+            }
+
+            settings.DATA_CONTENT new_content = new settings.DATA_CONTENT()
+            {
+                DATA = content
+            };
+
+            UILogs("Compressing").Wait();
+            string json = JsonConvert.SerializeObject(new_content);
+            UILogs($"Writing in {output}").Wait();
+            File.WriteAllText(output, json);
+
+            progress_bar(0).Wait();
+            new_content.DATA = null;
+            return (Task.CompletedTask);
+        }
+
+        private string clean_path(string path)
+        {
+            string new_path = "";
+
+            for (int i = 0; i < path.Length - 1; i++)
+            {
+                new_path += path[i + 1];
+            }
+            Console.WriteLine(new_path);
+            return (new_path);
+        }
+
+        private void launch_Click(object sender, EventArgs e)
+        {
+            string dump = "DUMP.mis";
+            progress_bar(0);
+
+            if (get_folder() == 0)
+            {
+                dump = $"{directory.SelectedPath.Split('\\')[count_char(directory.SelectedPath, '\\')]}.mis";
+                compress(dump).Wait();
+                UILogs($"Files compressed").Wait();
+                boxer($"Files compressed check: {dump}", "Success", MessageBoxIcon.Information).Wait();
+            } else
+            {
+                UILogs("Sorry but i can't compress nothing").Wait();
+            }
+        }
+
+        private int get_file()
+        {
+            file_mist.ShowDialog();
+            if (file_mist.FileName.Length == 0)
+                return (-1);
+            else
+                UILogs(crop(file_mist.FileName)).Wait();
+
+            return (0);
+        }
+
+        private Task generate_directory(string key)
+        {
+            int slashes = count_char(key, '\\');
+            string dir = "";
+
+            for (int i = 0; i < (slashes - 1); i++)
+            {
+                dir += $"{key.Split('\\')[i + 1]}\\";
+                Console.WriteLine(dir);
+
+                if (Directory.Exists(dir))
+                {
+                    UILogs($"Directory found :: {dir}");
+                } else
+                {
+                    UILogs($"Creating directory :: {dir}");
+                    Directory.CreateDirectory(dir);
+                    UILogs($"Directory created :: {dir}");
+                }
+            }
+
+            return (Task.CompletedTask);
+        }
+
+        private Task decompress()
+        {
+            string mist = File.ReadAllText(file_mist.FileName);
+            int progress = 0;
+            settings.DATA_CONTENT content = JsonConvert.DeserializeObject<settings.DATA_CONTENT>(mist);
+            var keys = content.DATA.Keys.ToArray();
+            var values = content.DATA.Values.ToArray();
+            int total = keys.Length;
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                progress = (i * 100) / keys.Length;
+                progress_bar(progress).Wait();
+                generate_directory(keys[i]).Wait();
+
+                if (File.Exists(keys[i]) == false)
+                {
+                    UILogs($"Extracting file :: {keys[i]}").Wait();
+                    File.WriteAllBytes(clean_path(keys[i]), values[i]);
+                    UILogs($"File extracted :: {keys[i]}").Wait();
+                } else
+                {
+                    UILogs($"File already present :: {keys[i]}").Wait();
+                }
+            }
+            progress_bar(0).Wait();
+            UILogs("Files extracted").Wait();
+
+            return (Task.CompletedTask);
+        }
+
+        private void uncompress_Click(object sender, EventArgs e)
+        {
+            progress_bar(0);
+            if (get_file() == 0)
+            {
+                UILogs($"Uncompressing {file_mist.FileName}").Wait();
+                decompress().Wait();
+                boxer("Files extracted", "Success", MessageBoxIcon.Information).Wait();
+            } else
+            {
+                UILogs("Sorry but i can't uncompress nothing").Wait();
+            }
+        }
+
+        private void reduce_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
